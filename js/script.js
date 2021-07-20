a096d0f4ac0da140f978513f75f4870cb403e544
thorongil <officialantonio@outlook.it>
2021-07-20 16:54:03 +0200

    add preview of the website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    console.log("Welcome in the Antonio's website.");
+    var previewContainer = document.getElementById("previewContainer");
+    var previewContinueButton = document.getElementById("previewContinueButton");
+    previewContinueButton.onclick = function () {
+        // previewContainer.classList.add("hidden");
+        // console.log("Preview hidden correctly");
+        alert("The page is not yet implemented.\nThe website will be available very soon.\nEnjoy this typewriting animation.")
+    };
+}
+
